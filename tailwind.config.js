8336ccb893213192bd2b28e10ebcf0c8c4eca424
Rizky Tegar <55475891+rizkytegar@users.noreply.github.com>
2023-03-29 16:09:28 +0700

    update new

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/components/**/*.{ts,tsx}', 
+    './src/pages/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: { 
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
